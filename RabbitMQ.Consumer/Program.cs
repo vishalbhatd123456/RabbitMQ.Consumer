167607319c7bb25adb4f32abdaca3c7ef595500b
vishalbhatd123456 <vishaldbhat123456@gmail.com>
2023-02-25 13:07:11 +0530

    Added consumer code

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+               Uri =new  Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("demo-queue", true, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+            channel.BasicConsume("demo-queue", true, consumer);
+            Console.ReadLine();
+        }
+    }
+}
+
